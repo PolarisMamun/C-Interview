01ff0cc4df433fd6fce10bb2b8ff35c90a7b5680
Munsi Abdul Al Mamun <munsiabd.almamun@petronas.com>
2024-03-24 01:07:19 +0800

    Learn Functions

---
+++ Functions/Function.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Functions
+{
+    public static class Function
+    {
+        public static void DisplayMessage()
+        {
+            Console.WriteLine("Hello World");
+        }
+        //In this example, the function DisplayMessage is declared with the void keyword, indicating that it does not return a value. The function contains one statement that prints the message "Hello World!" to the console.
+
+        public static string ConvertToUpperCase(string input)
+        {
+            return input.ToUpper();
+        }
+
+        public static int Add(int x , int y)
+        {
+            return x + y;
+        }
+    }
+}
---
+++ Functions/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using static Functions.Function;
+namespace Functions
+{
+    public class Program
+    {
+        //In C#, a function is a block of code that performs a specific task. Functions help to organize code, promote reusability, and make code more readable and maintainable.
+
+       // A function consists of a name, a set of parameters, and a body.The body of the function contains the code that performs the desired action.
+        //In C#, you can define functions in different files and call them in the main method by using namespaces and using directives.
+        static void Main(string[] args)
+        {
+            DisplayMessage();
+
+            Console.Write("Enter a Word: ");
+            string input = Console.ReadLine();
+
+            string upperMessage = ConvertToUpperCase(input);
+            Console.WriteLine(upperMessage);
+
+            Console.Write("Enter 1st Number: ");
+            int x = Int32.Parse(Console.ReadLine());
+
+            Console.Write("Enter 2nd Number: ");
+            int y = Int32.Parse(Console.ReadLine());
+
+            int sum = Add(x, y);
+
+            Console.WriteLine(sum);
+
+
+            Console.ReadLine();
+        }
+    }
+}
